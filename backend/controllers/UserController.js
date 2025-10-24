6a97ae1a9385f9f50a81ff461f255c349093221e
liedsonlm7 <liedsonlm7@gmail.com>
2025-10-24 11:16:05 -0300

    feat(controller): create router to register a user

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,73 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+module.exports = class UserController {
+    static async register(req, res) {
+        const { name, email, phone, password, confirmpassword } = req.body
+
+        // validations
+        if (!name) {
+            res.status(400).json({ message: 'O nome é obrigatório' })
+            return
+        }
+
+        if (!email) {
+            res.status(400).json({ message: 'O e-mail é obrigatório' })
+            return
+        }
+
+        if (!phone) {
+            res.status(400).json({ message: 'O telefone é obrigatório' })
+            return
+        }
+
+        if (!password) {
+            res.status(400).json({ message: 'A senha é obrigatória' })
+            return
+        }
+
+        if (!confirmpassword) {
+            res.status(400).json({ message: 'A confirmação de senha é obrigatória' })
+            return
+        }
+
+        if (confirmpassword !== password) {
+            res.status(400).json({ message: "A senha e a confirmação de senha precisam ser iguais!" })
+            return
+        }
+
+        // check if user exists
+        const userExists = await User.findOne({ email: email })
+
+        if (userExists) {
+            res.status(400).json({ message: "Por favor, utilize outro e-mail!" })
+            return
+        }
+
+        // create a password
+        const salt = await bcrypt.genSalt(12);
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        // create a user
+        const user = new User({
+            name: name,
+            email: email,
+            phone: phone,
+            password: passwordHash,
+        });
+
+        try 
+        {
+            const newUser = await user.save();
+            res.status(201).json({
+                message: 'Usuário cadastrado com sucesso!',
+                newUser
+            })
+        } 
+        catch (error) 
+        {
+            res.status(500).json({ message: error })
+        }
+
+    };
+};
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+
+const UserRoutes = require('./routes/UserRoutes');
 
 const cors = require('cors');
 
@@ -16,7 +18,7 @@ app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(express.static('public'));
 
 // Routes
-
+app.use('/users', UserRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Servidor iniciado na porta ${PORT}`)
