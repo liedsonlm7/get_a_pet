f6238848b51d396a6746cb99a1240bece1ce0685
liedsonlm7 <liedsonlm7@gmail.com>
2025-10-24 11:32:14 -0300

    feat(helper): create helper to return token JWT

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const createUserToken = require('../helpers/create-user-token');
 
 module.exports = class UserController {
     static async register(req, res) {
@@ -59,10 +60,8 @@ module.exports = class UserController {
         try 
         {
             const newUser = await user.save();
-            res.status(201).json({
-                message: 'Usuário cadastrado com sucesso!',
-                newUser
-            })
+            
+            await createUserToken(newUser, req, res);
         } 
         catch (error) 
         {
