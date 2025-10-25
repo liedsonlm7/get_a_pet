3bdfbc7519800e04be2f36266c88792f045b4a43
liedsonlm7 <liedsonlm7@gmail.com>
2025-10-25 12:27:52 -0300

    feat(controller): create function to get all pets

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -79,4 +79,13 @@ module.exports = class PetController {
         }
 
     }
+
+    static async getAll(req, res) {
+
+        const pets = await Pet.find().sort('-createdAt');
+
+        res.status(200).json({
+            pets: pets,
+        })
+    }
 }
