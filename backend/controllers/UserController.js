ce2777b68ef82cc9dc6b264a99c6eebd6d76c2f4
liedsonlm7 <liedsonlm7@gmail.com>
2025-10-25 10:55:00 -0300

    feat(controller): create route to update user

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 // helpers
 const createUserToken = require('../helpers/create-user-token');
 const getToken = require('../helpers/get-token');
+const getUserByToken = require('../helpers/get-user-by-token');
 
 module.exports = class UserController {
     static async register(req, res) {
@@ -137,9 +138,79 @@ module.exports = class UserController {
     }
 
     static async editUser(req, res) {
-        res.status(200).json({
-            message: 'Deu certo update!'
-        })
-        return
+        
+        const id = req.params.id;
+
+        // check if user exists
+        const token = getToken(req);
+        const user = await getUserByToken(token);
+
+        const { name, email, phone, password, confirmpassword } = req.body;
+
+        let image = '';
+
+        // validations
+         if (!name) {
+            res.status(400).json({ message: 'O nome é obrigatório' })
+            return
+        }
+
+        user.name = name;
+
+        if (!email) {
+            res.status(400).json({ message: 'O e-mail é obrigatório' })
+            return
+        }
+
+        // check if email has already taken
+        const userExists = await User.findOne({email: email});
+
+        if(user.email !== email && userExists) {
+            res.status(422).json({
+                message: 'Por favor, utilize outro e-mail!'
+            })
+            return
+        }
+
+        user.email = email;
+
+        if (!phone) {
+            res.status(400).json({ message: 'O telefone é obrigatório' })
+            return
+        }
+
+        user.phone = phone;
+
+        if (password !== confirmpassword) {
+            res.status(400).json({ message: "A senha e a confirmação de senha precisam ser iguais!" })
+            return
+        } else if(password === confirmpassword && password != null) {
+            // creating password
+            const salt = await bcrypt.genSalt(12);
+            const passwordHash = await bcrypt.hash(password, salt);
+
+            user.password = passwordHash;
+        }
+
+        try 
+        {
+           // returns user upadated data
+           const upadatedUser = await User.findOneAndUpdate(
+              { _id: user._id },
+              { $set: user},
+              { new: true },
+           )
+
+           res.json(200).json({
+               message: "Usuário atualizado com sucesso!",
+           })
+        } 
+        catch (error) 
+        {
+            res.status(500).json({ message: error })
+        }
+
+
+
     }
 };
