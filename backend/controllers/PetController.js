a6c969aa2714c6fb6205546896f79d496c980f3f
liedsonlm7 <liedsonlm7@gmail.com>
2025-10-27 08:55:25 -0300

    feat(controller): remove pet

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -134,4 +134,35 @@ module.exports = class PetController {
             pet: pet,
         })
     }
+
+    static async removePetById(req, res) {
+        const id = req.params.id;
+
+        // check if id is valid
+        if(!ObjectId.isValid(id)) {
+            res.status(422).json({ message: 'ID inválido!' });
+            return
+        }
+
+        // check if pet exists
+        const pet = await Pet.findOne({_id: id});
+
+        if(!pet) {
+            res.status(404).json({message: 'Pet não encontrado!' })
+        }
+
+        // check if logged in user registered the pet
+        const token = getToken(req);
+        const user = await getUserByToken(token);
+
+        if(pet.user._id.toString() !== user._id.toString()) {
+            res.status(422).json({message: 'Houve um problema em processar sua solicitação, tente novamente mais tarde!' })
+        }
+
+        await Pet.findByIdAndDelete(id);
+
+        res.status(204).json({ message: 'Pet removido com sucesso!'})
+
+
+    }
 }
